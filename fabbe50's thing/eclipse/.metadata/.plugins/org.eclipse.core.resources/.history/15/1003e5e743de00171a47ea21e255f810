4128d9c599e7369cd754c76b4d36d5256b9e5cc0
Squee_Z <finnmiccol@gmail.com>
2017-12-13 20:12:07 -0600

    First Steps

--- fabbe50's thing/eclipse/.metadata/.plugins/org.eclipse.core.resources/.history/15/1003e5e743de00171a47ea21e255f810
+++
@@ -1,5 +0,0 @@
-package net.modfun.proxy;
-
-public class ClientProxy extends CommonProxy {
-
-}
--- fabbe50's thing/src/main/java/net/modfun/Reference.java
+++
@@ -1,9 +0,0 @@
-package net.modfun;
-
-public class Reference {
-
-	public static final String CLIENT_PROXY_CLASS = "net.modfun.proxy.ClientProxy";
-	public static final String COMMON_PROXY_CLASS = "net.modfun.proxy.CommonProxy";
-	public static final String MOD_ID = "modfun";
-
-}
